8a4beafc0d5b76da9c9cbf25d770815eea654792
Aler4 <alerch5556@gmail.com>
2022-10-19 00:37:38 +0300

    add preview section

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,7 @@
-import {addBurger} from "./modules/burger.js"
+import {addBurger} from "./modules/burger.js";
+import {previewSlider} from './modules/previewSlider.js';
 
+previewSlider();
 addBurger();
 window.addEventListener('resize', addBurger);
 console.log('asd')
---
+++ src/js/modules/previewSlider.js
@@ -0,0 +1,17 @@
+import Swiper, { Navigation, Pagination } from 'swiper';
+export const previewSlider = () => {
+    const slider = new Swiper('.swiper', {
+        modules: [Navigation, Pagination],
+        direction: 'horizontal',
+        loop: true,
+        slidesPerView: 1,
+        spaceBetween: 0,
+        pagination: {
+            el: '.swiper-pagination',
+        },
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+    });
+};
