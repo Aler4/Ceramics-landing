abde911739d4a6951385598940d314fb278d7ae5
Aler4 <alerch5666@gmail.com>
2022-10-27 16:46:45 +0300

    footer

--- src/js/modules/blogSlider.js
+++ src/js/modules/blogSlider.js
@@ -3,7 +3,22 @@ export const blogSlider = () => {
     return new Swiper('.blog__cards', {
         direction: 'horizontal',
         loop: true,
-        slidesPerView: 4.2,
         spaceBetween: 30,
+        breakpoints: {
+            960: {
+                slidesPerView: 2,
+                spaceBetween: 15,
+            },
+            1350: {
+                slidesPerView: 3,
+                spaceBetween: 30,
+            },
+            1840: {
+                spaceBetween: 30,
+                slidesPerView: 4.2,
+
+            },
+        },
     });
+
 };
